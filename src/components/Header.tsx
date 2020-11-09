3f627fb2928709bf116fb902c0b15f4f8ad10fd7
ArthurBiasibetti <arthurbiasibettifarias@gmail.com>
2020-11-09 15:02:23 -0300

    Componente Header craido

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import '../styles/components/header.css'
+
+import Logo from '../images/Logo.svg'
+import mano from '../images/mano.jpg'
+
+import { GoBell } from 'react-icons/go'
+import { BsCaretDownFill } from 'react-icons/bs'
+
+export default function Header () {
+  return(
+    <header className='cabecalho'>
+      <div className='left-content'>
+        <img className='clicklable' src={Logo} alt="Athena's Logo"/>
+        <ul>
+          <li className='active clicklable'>Tarefas</li>
+          <li className='clicklable'>Portfólio</li>
+        </ul>
+      </div>
+      <div className="right-content">
+        <GoBell className='clicklable' size={24} color="#f2faf8" />
+        <div className="image-content">
+          <img src={mano} alt="foto de usuario"/>
+        </div>
+        <BsCaretDownFill className='clicklable' size={24} color='#f2faf8' />
+      </div>
+    </header>
+  );
+}
--- src/pages/Tarefas.tsx
+++ src/pages/Tarefas.tsx
@@ -1,34 +1,15 @@
 import React from 'react';
 
+import Header from '../components/Header'
 import '../styles/pages/tarefas.css';
 
-import { GoBell } from 'react-icons/go'
-import { BsCaretDownFill } from 'react-icons/bs'
 import SimpleBar from 'simplebar-react';
-
-import Logo from '../images/Logo.svg'
-import mano from '../images/mano.jpg'
 
 function Tarefas() {
   return (
     <div id="app-tarefas">
       <div className="content-wrapper">
-        <header className='cabecalho'>
-          <div className='left-content'>
-            <img className='clicklable' src={Logo} alt="Athena's Logo"/>
-            <ul>
-              <li className='active clicklable'>Tarefas</li>
-              <li className='clicklable'>Portfólio</li>
-            </ul>
-          </div>
-          <div className="right-content">
-            <GoBell className='clicklable' size={24} color="#f2faf8" />
-            <div className="image-content">
-              <img src={mano} alt="foto de usuario"/>
-            </div>
-            <BsCaretDownFill className='clicklable' size={24} color='#f2faf8' />
-          </div>
-        </header>
+        <Header />
         <aside>
           <SimpleBar style={{maxHeight: '100%'}}>
             <ul className='turma-list'>
