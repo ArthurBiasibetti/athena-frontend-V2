487eeccf05e2b79505adfe17cb484601051c2e35
ArthurBiasibetti <arthurbiasibettifarias@gmail.com>
2020-11-09 16:18:30 -0300

    Portfolio inicial

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 
+import '../styles/pages/portfolio.css'
+
 import Aside from '../components/Aside';
 import Header from '../components/Header';
 
+// import SimpleBar from 'simplebar-react';
+
 function Portfolio () {
   return(
-    <div className="app-portfilio">
+    <div id="app-portfilio">
       <div className="content-wrapper">
         <Header />
         <Aside />
+        <main>
+           <div className="file-viewer">
+             <h1>Teste provisorio</h1>
+           </div>
+        </main>
       </div>
     </div>
   );
